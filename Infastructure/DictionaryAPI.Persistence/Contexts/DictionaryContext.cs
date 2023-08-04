4e70011b41e4b746b10cd8bcee07183c540dd1ee
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-04 17:04:08 +0300

    Create DictionaryContext and Database Connection

---
+++ Infastructure/DictionaryAPI.Persistence/Contexts/DictionaryContext.cs
@@ -0,0 +1,26 @@
+﻿using DictionaryAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace DictionaryAPI.Persistence.Contexts
+{
+    public class DictionaryContext : DbContext
+    {
+
+        ConfigurationManager configurationManager = new();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            configurationManager.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            optionsBuilder.UseSqlServer(configurationManager.GetConnectionString("MSSQL"));
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
