62b10b0aed8acdd6c1b3aa56acfcd12065fe40b8
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-04 17:16:47 +0300

    Create Title Entity

---
+++ Core/DictionaryAPI.Domain/Entities/Title.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Domain.Entities
+{
+    public class Title : BaseEntity
+    {
+        public string Content { get; set; }
+        public string Slug { get; set; }
+
+        public ICollection<Entry> Entries { get; set; }
+
+    }
+}
