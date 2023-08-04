128339ebd2bc7873b1bc062bc0568507de310a24
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-04 17:59:42 +0300

    Create Data Access Layers

---
+++ Core/DictionaryAPI.Application/Abstracts/DAL/IUserDal.cs
@@ -0,0 +1,14 @@
+﻿using DictionaryAPI.Application.Abstracts.Repository;
+using DictionaryAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Application.Abstracts.DAL
+{
+    public interface IUserDal : IEntityRepositoryBase<User>
+    {
+    }
+}
---
+++ Infastructure/DictionaryAPI.Persistence/Concretes/DAL/UserDal.cs
@@ -0,0 +1,15 @@
+﻿using DictionaryAPI.Application.Abstracts.DAL;
+using DictionaryAPI.Domain.Entities;
+using DictionaryAPI.Persistence.Concretes.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Persistence.Concretes.DAL
+{
+    public class UserDal : EntityRepositoryBase<User>, IUserDal
+    {
+    }
+}
