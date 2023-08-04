9ac7e6c92586d129699cc13bd83cb0cac259e298
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-04 17:53:59 +0300

    Create EntityRepository

---
+++ Core/DictionaryAPI.Application/Abstracts/IEntityRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Application.Abstracts
+{
+    public interface IEntityRepositoryBase<T>
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T GetById(Guid id);
+        
+    }
+}
---
+++ Infastructure/DictionaryAPI.Persistence/Concretes/EntityRepositoryBase.cs
@@ -0,0 +1,60 @@
+﻿using DictionaryAPI.Application.Abstracts;
+using DictionaryAPI.Domain.Entities;
+using DictionaryAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Persistence.Concretes
+{
+    public class EntityRepositoryBase<T> : IEntityRepositoryBase<T>
+        where T: BaseEntity, new()
+        //Generic Type must be instance of BaseEntity and can be created new instance of Generic Type with new()
+    {
+        public void Add(T entity)
+        {
+            using (DictionaryContext context = new())
+            {
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            using (DictionaryContext context = new())
+            {
+                context.Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            using (DictionaryContext context = new())
+            {
+                return context.Set<T>().ToList();
+            }
+        }
+
+        public T GetById(Guid id)
+        {
+            using (DictionaryContext context = new())
+            {
+                return context.Set<T>().FirstOrDefault(e => e.Id == id);
+            }
+        }
+
+        public void Update(T entity)
+        {
+            using (DictionaryContext context = new())
+            {
+                context.Update(entity);
+                context.SaveChanges();
+            }
+        }
+    }
+}
