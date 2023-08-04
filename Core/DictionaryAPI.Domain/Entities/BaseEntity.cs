fa442ae0409f370462ab1bcf117acd4fcc42d1d7
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-04 15:00:29 +0300

    Create BaseEntity

---
+++ Core/DictionaryAPI.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsVisible { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
