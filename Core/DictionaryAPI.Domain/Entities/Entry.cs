f9c6e4a8321c6979b547a84ac1f9da196ef7dd88
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-04 17:16:53 +0300

    Create Entry Entity

---
+++ Core/DictionaryAPI.Domain/Entities/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Domain.Entities
+{
+    public class Entry : BaseEntity
+    {
+        public string Content { get; set; }
+        public Guid UserId { get; set; }
+        public Guid TitleId { get; set; }
+        public User User { get; set; }
+        public Title Title { get; set; }
+    }
+}
