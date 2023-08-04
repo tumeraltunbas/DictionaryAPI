60d3ff58eb694e4d560b15ab798d2fcc54baea1d
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-04 18:04:17 +0300

    Create SignUpDto

---
+++ Core/DictionaryAPI.Application/DTOs/SignUpDto.cs
@@ -0,0 +1,18 @@
+﻿using DictionaryAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Application.DTOs
+{
+    public class SignUpDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
