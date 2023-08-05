2f024af4306d73293595ab27d8c167d1634a7eef
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-05 16:29:24 +0300

    Create GenerateHash helper

---
+++ Core/DictionaryAPI.Application/Abstracts/Security/Hash/IHashHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Application.Abstracts.Security.Hash
+{
+    public interface IHashHelper
+    {
+        Tuple<byte[], byte[]> GenerateHash(string password);
+    }
+}
---
+++ Infastructure/DictionaryAPI.Infastructure/Security/Hash/HashHelper.cs
@@ -0,0 +1,24 @@
+﻿using DictionaryAPI.Application.Abstracts.Security.Hash;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Infastructure.Security.Hash
+{
+    public class HashHelper : IHashHelper
+    {
+        public Tuple<byte[], byte[]> GenerateHash(string password)
+        {
+            HMACSHA256 algorithm = new();
+
+            byte[] salt = algorithm.Key;
+            byte[] hash = algorithm.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            return new Tuple<byte[], byte[]>(salt, hash);
+
+        }
+    }
+}
