a274ea4e3f8187405fe6d0d49055fc75b3bafd70
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-17 19:08:23 +0300

    Create SignInDto

---
+++ Core/DictionaryAPI.Application/DTO/DTOValidators/SignInDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using DictionaryAPI.Application.DTO.DTOs;
+using DictionaryAPI.Application.Utils.Constants;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Application.DTO.DTOValidators
+{
+    public class SignInDtoValidator : AbstractValidator<SignInDto>
+    {
+        public SignInDtoValidator()
+        {
+            RuleFor(s => s.Email).NotNull().WithMessage(Message.EmailNotNull);
+            RuleFor(s => s.Email).EmailAddress().WithMessage(Message.InvalidEmail);
+
+            RuleFor(s => s.Password).NotNull().WithMessage(Message.PasswordNotNull);
+        }
+    }
+}
