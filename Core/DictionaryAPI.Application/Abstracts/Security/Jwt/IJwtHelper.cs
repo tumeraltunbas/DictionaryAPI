984e10486e5f5ea18d43bc221a4ad2096e5aea8c
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-17 19:08:08 +0300

    Create JwtHelper

---
+++ Core/DictionaryAPI.Application/Abstracts/Security/Jwt/IJwtHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DictionaryAPI.Domain.Entities;
+
+namespace DictionaryAPI.Application.Abstracts.Security.JWT
+{
+    public interface IJwtHelper
+    {
+        string GenerateJwt(User user);
+    }
+}
---
+++ Infastructure/DictionaryAPI.Infastructure/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,55 @@
+﻿using DictionaryAPI.Application.Abstracts.Security.JWT;
+using DictionaryAPI.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Infastructure.Security.Jwt
+{
+    public class JwtHelper : IJwtHelper
+    {
+        IConfiguration _configuration;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateJwt(User user)
+        {
+            //Get symmetric of security key. From Microsoft.IdentityModel.Tokens
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("Jwt:SecurityKey"))); //Get bytes of security key
+
+            //Create SigningCredentials. From Microsoft.IdentityModel.Tokens
+            SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha256); //Encryption of security key.
+
+            //Claims
+            Claim[] claims = new Claim[]
+            {
+                new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new(ClaimTypes.Email, user.Email.ToString())
+            };
+
+            //JWT configuration. From System.IdentityModel.Tokens.Jwt
+            JwtSecurityToken securityToken = new(
+                audience: _configuration.GetValue<string>("Jwt:Audience"),
+                issuer: _configuration.GetValue<string>("Jwt:Issuer"),
+                expires: DateTime.UtcNow.AddMinutes(_configuration.GetValue<int>("Jwt:ExpiresInMinutes")),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: signingCredentials,
+                claims: claims
+                );
+
+            //Creating token
+            JwtSecurityTokenHandler jwtHandler = new();
+            return jwtHandler.WriteToken(securityToken);
+
+        }
+    }
+}
