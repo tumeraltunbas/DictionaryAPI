769ee6407030de40c83841cbc51a35d20ad7cf49
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-17 17:30:11 +0300

    Create UtilService

---
+++ Core/DictionaryAPI.Application/Utils/IUtilService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Application.Utils
+{
+    public interface IUtilService
+    {
+        string GenerateRandomString(int size);
+        string GenerateEmailVerificationLink();
+    }
+}
---
+++ Infastructure/DictionaryAPI.Infastructure/Utils/UtilService.cs
@@ -0,0 +1,46 @@
+﻿using DictionaryAPI.Application.Utils;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Infastructure.Utils
+{
+    public class UtilService : IUtilService
+    {
+
+        IConfiguration _configuration;
+
+        public UtilService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateRandomString(int size)
+        {
+            Random random = new();
+            StringBuilder sb = new();
+
+            string text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            for (int i = 0; i < size; i++)
+            {
+                int randomNumber = random.Next(text.Length);
+                sb.Append(text[randomNumber]);
+            }
+
+            return sb.ToString();
+
+        }
+        public string GenerateEmailVerificationLink()
+        {
+            string domain = _configuration.GetValue<string>("DevDomain");
+            string token = GenerateRandomString(25);
+
+            return $"{domain}/api/email/verification/verify?emailVerificationToken={token}";
+        }
+
+    }
+}
