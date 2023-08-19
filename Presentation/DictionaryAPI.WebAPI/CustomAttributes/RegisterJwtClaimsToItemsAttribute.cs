c670eae343291ca1cd98ed1366b62757be5d9db3
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-19 17:45:00 +0300

    Create RegisterJwtClaimsToItemsAttribute

---
+++ Presentation/DictionaryAPI.WebAPI/CustomAttributes/RegisterJwtClaimsToItemsAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace DictionaryAPI.WebAPI.CustomAttributes
+{
+    public class RegisterJwtClaimsToItemsAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                string id = context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+                string email = context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
+                context.HttpContext.Items["Id"] = id;
+                context.HttpContext.Items["Email"] = email;
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+    }
+}
