a2d7681927d38b44e66081ee07db29f660847955
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-20 18:55:24 +0300

    Create EntryFavorite Entity

---
+++ Core/DictionaryAPI.Domain/Entities/EntryFavorite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Domain.Entities
+{
+    public class EntryFavorite: BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public Guid EntryId { get; set; }
+        public User User { get; set; }
+        public Entry Entry { get; set; }
+
+        public EntryFavorite(Guid userId, Guid entryId)
+        {
+            UserId = userId;
+            EntryId = entryId;
+        }
+
+    }
+}
