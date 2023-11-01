c3b3d7578ca0903ebd5f4a8c84f4df5dc6d12fde
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-11-02 00:09:34 +0100

    Create StorageService for file upload

---
+++ Core/DictionaryAPI.Application/Abstracts/Services/StorageService/IStorageService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Application.Abstracts.Services.StorageService
+{
+    public interface IStorageService
+    {        
+        /*
+         Modification List:
+         I must upload both single file and multiple files and I must restrict this when I call the function.
+         I must send the allowed file types as a parameter.
+         */
+
+        string UploadFile(IFormFile file, string uploadPath);
+    }
+}
---
+++ Infastructure/DictionaryAPI.Infastructure/Services/Storage/StorageService.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Core;
+using DictionaryAPI.Application.Abstracts.Services.StorageService;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAPI.Infastructure.Services.Storage
+{
+    public class StorageService : IStorageService
+    {
+        public string UploadFile(IFormFile file, string uploadPath)
+        {
+
+            //Mimetype check will be added.
+
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            Random r = new();
+
+            string randomFileName = $"{r.Next()}{Path.GetExtension(file.FileName)}";
+
+            string filePath = Path.Combine(uploadPath, $"{randomFileName}");
+
+            FileStream fileStream = new(
+                path: filePath,
+                mode: FileMode.Create,
+                access: FileAccess.Write,
+                share: FileShare.None
+                );
+
+            file.CopyTo(fileStream);
+            fileStream.Flush();
+
+            return randomFileName;
+
+        }
+    }
+}
